4a291cae5e13f7679886d51297f6f04eb6f99a97
joshualarido <joshualarido@gmail.com>
2025-05-06 18:45:19 +0700

    Added state remembering for nav buttons Added 404 not found page for unknown routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Camera from './pages/Camera'
 import Collection from './pages/Collection'
 import Crafts from './pages/Crafts'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -12,9 +13,14 @@ function App() {
       <Sidebar/>
       <div className="w-full bg-gray-100 px-6 py-8 z-0">
         <Routes>
-          <Route path="/" element={<Camera />} />
+          <Route path="/" element={<Navigate to="/camera" replace />} />
+
+          <Route path="/camera" element={<Camera />} />
           <Route path="/collection" element={<Collection />} />
           <Route path="/crafts" element={<Crafts />} />
+          <Route path="/404" element={<NotFound />} />
+          
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
       </div>
     </div>
