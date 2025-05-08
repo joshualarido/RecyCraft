110fe65d1cd8f9c7bc1d91ec6dd459e234c87edb
joshualarido <joshualarido@gmail.com>
2025-05-08 10:41:26 +0700

    Fixed craft details page progress bar and design consistency problems

--- src/components/BigProgressBar.jsx
+++ src/components/BigProgressBar.jsx
@@ -1,11 +1,11 @@
 const BigProgressBar = ({progress}) => {
   return (
       <>
-        <div className="w-full h-16 rounded-xl bg-white shadow-xl flex items-center px-4 gap-4 font-bold">
+        <div className="w-full h-full rounded-xl bg-white shadow-lg flex items-center p-4 gap-4 font-bold">
             Progress
             <div className="flex-1 h-3 rounded bg-gray-200 ">
             <div className="flex h-3 rounded bg-emerald-500"
-                            style={{ width: `${progress * 13.5}px`}}>
+                            style={{ width: `${progress}%`}}>
             </div>
             </div>
            {progress}%
--- src/components/CraftStepCard.jsx
+++ src/components/CraftStepCard.jsx
@@ -5,7 +5,7 @@ const CraftStepCard = ({StepNumber, StepTitle, StepDescription, isClicked, onCli
   return(
         <>
         <div onClick={onClick} 
-             className={`w-full min-h-36 rounded-xl shadow-xl flex flex-col px-6 py-4 transition-colors duration-300
+             className={`w-full min-h-36 rounded-xl shadow-lg flex flex-col px-6 py-4 transition-colors duration-300
                          ${isClicked ? 'bg-green-200' : 'bg-white'}`}>
           
 
