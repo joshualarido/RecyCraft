f47ba6c232ba0777d449bc5351d476cb9df2a763
joshualarido <joshualarido@gmail.com>
2025-05-16 22:59:20 +0700

    link change final

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.post(`${import.meta.env.VITE_API_URL}/gemini/text`, async (req, res) => {
+app.post(`${process.env.RAILWAY_LINK}/gemini/text`, async (req, res) => {
     const prompt = req.body.prompt;
     const image = req.body.image;
 
@@ -30,7 +30,7 @@ app.post(`${import.meta.env.VITE_API_URL}/gemini/text`, async (req, res) => {
     }
 });
 
-app.post(`${import.meta.env.VITE_API_URL}/gemini/image`, async (req, res) => {
+app.post(`${process.env.RAILWAY_LINK}/gemini/image`, async (req, res) => {
     const prompt = req.body.prompt;
 
     try {
